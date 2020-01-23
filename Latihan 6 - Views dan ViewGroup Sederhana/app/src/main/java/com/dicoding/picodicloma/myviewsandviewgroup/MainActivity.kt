1ee89d17d1fed448881c726ea833113d8a527481
fal7h3 <itsalfath123@gmail.com>
2020-01-23 23:25:28 +0700

    Added Latihan 6

---
+++ Latihan 6 - Views dan ViewGroup Sederhana/app/src/main/java/com/dicoding/picodicloma/myviewsandviewgroup/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.dicoding.picodicloma.myviewsandviewgroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Google Pixel"
+    }
+}

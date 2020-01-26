0347ab3a81a3e48afd632ffa8aa185e9e5408709
fal7h3 <itsalfath123@gmail.com>
2020-01-27 00:56:05 +0700

    Completed Latihan 6

---
+++ Latihan 6 - Views dan ViewGroup Sederhana/app/src/main/java/com/dicoding/picodicloma/myviewsandviewgroup/MyButton.kt
@@ -0,0 +1,2 @@
+package com.dicoding.picodicloma.myviewsandviewgroup
+
---
+++ Latihan 6 - Views dan ViewGroup Sederhana/app/src/main/java/com/dicoding/picodicloma/myviewsandviewgroup/MyButtonAndTextActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodicloma.myviewsandviewgroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyButtonAndTextActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_button_and_text)
+    }
+}

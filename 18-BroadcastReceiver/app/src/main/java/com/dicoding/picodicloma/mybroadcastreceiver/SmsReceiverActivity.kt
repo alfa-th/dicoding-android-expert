01e1761a72ceb1dcc86d85da75aa40dd5516a882
fal7h3 <itsalfath123@gmail.com>
2020-02-05 22:43:57 +0700

    Update

---
+++ 18-BroadcastReceiver/app/src/main/java/com/dicoding/picodicloma/mybroadcastreceiver/SmsReceiverActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodicloma.mybroadcastreceiver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SmsReceiverActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sms_receiver)
+    }
+}
